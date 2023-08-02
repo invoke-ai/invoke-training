fc2e2c6c01f2541a5e057ce991cdc5184ffefd88
Ryan Dick <ryanjdick3@gmail.com>
2023-08-02 16:13:37 -0400

    Add convert_lora_state_dict_to_kohya_format_sd1(...).

--- src/invoke_training/lora/injection/stable_diffusion_v1.py
+++ src/invoke_training/lora/injection/stable_diffusion_v1.py
@@ -1,6 +1,7 @@
+import typing
+
 import torch
 from diffusers.models import Transformer2DModel, UNet2DConditionModel
-from diffusers.models.attention import FeedForward
 from diffusers.models.lora import LoRACompatibleLinear
 
 from invoke_training.lora.injection.lora_layer_collection import LoRALayerCollection
@@ -18,18 +19,56 @@ def inject_lora_into_unet_sd1(unet: UNet2DConditionModel) -> LoRALayerCollection
         LoRALayerCollection: The LoRA layers that were added to the UNet.
     """
 
-    # This combination of including Transformer2DModel descendants and excluding FeedForward descendants is designed to
-    # mimic how kohya_ss handles Linear LoRA layers. After initial testing, this will be expanded to include the
-    # FeedForward layers as well.
     lora_layers = inject_lora_layers(
         module=unet,
         lora_map={torch.nn.Linear: LoRALinearLayer, LoRACompatibleLinear: LoRALinearLayer},
         include_descendants_of={Transformer2DModel},
-        exclude_descendants_of={FeedForward},
+        exclude_descendants_of=None,
+        prefix="lora_unet",
     )
 
     return lora_layers
 
 
-def convert_lora_state_dict_to_kohya_format_sd1(state_dict):
-    pass
+def convert_lora_state_dict_to_kohya_format_sd1(
+    state_dict: typing.Dict[str, torch.Tensor]
+) -> typing.Dict[str, torch.Tensor]:
+    """Convert a Stable Diffusion v1 LoRA state_dict from internal invoke-training format to kohya_ss format.
+
+    Args:
+        state_dict (typing.Dict[str, torch.Tensor]): LoRA layer state_dict in invoke-training format.
+
+    Raises:
+        ValueError: If state_dict contains unexpected keys.
+        RuntimeError: If two input keys map to the same output kohya_ss key.
+
+    Returns:
+        typing.Dict[str, torch.Tensor]: LoRA layer state_dict in kohya_ss format.
+    """
+    new_state_dict = {}
+
+    # The following logic converts state_dict keys from the internal invoke-training format to kohya_ss format.
+    # Example conversion:
+    # from: 'lora_unet.down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q._down.weight'
+    # to:   'lora_unet_down_blocks_0_attentions_0_transformer_blocks_0_attn1_to_q.lora_down.weight'
+    for key, val in state_dict.items():
+        if key.endswith("._up.weight"):
+            key_start = key.removesuffix("._up.weight")
+            key_end = ".lora_up.weight"
+        elif key.endswith("._down.weight"):
+            key_start = key.removesuffix("._down.weight")
+            key_end = ".lora_down.weight"
+        elif key.endswith(".alpha"):
+            key_start = key.removesuffix(".alpha")
+            key_end = ".alpha"
+        else:
+            raise ValueError(f"Unexpected key in state_dict: '{key}'.")
+
+        new_key = key_start.replace(".", "_") + key_end
+
+        if new_key in new_state_dict:
+            raise RuntimeError("Multiple input keys map to the same kohya_ss key: '{new_key}'.")
+
+        new_state_dict[new_key] = val
+
+    return new_state_dict
--- tests/invoke_training/lora/injection/test_stable_diffusion_v1.py
+++ tests/invoke_training/lora/injection/test_stable_diffusion_v1.py
@@ -1,11 +1,16 @@
 import pytest
+import torch
 from diffusers.models import UNet2DConditionModel
 
-from invoke_training.lora.injection.stable_diffusion_v1 import inject_lora_into_unet_sd1
+from invoke_training.lora.injection.stable_diffusion_v1 import (
+    convert_lora_state_dict_to_kohya_format_sd1,
+    inject_lora_into_unet_sd1,
+)
 
 
 @pytest.mark.loads_model
-def test_inject_lora_into_unet_sd1():
+def test_inject_lora_into_unet_sd1_smoke():
+    """Smoke test of inject_lora_into_unet_sd1(...) on full SD 1.5 model."""
     unet = UNet2DConditionModel.from_pretrained(
         "runwayml/stable-diffusion-v1-5", subfolder="unet", local_files_only=True
     )
@@ -14,6 +19,75 @@ def test_inject_lora_into_unet_sd1():
 
     # These assertions are based on a manual check of the injected layers and comparison against the behaviour of
     # kohya_ss. They are included here to force another manual review after any future breaking change.
-    assert len(lora_layers) == 128
+    assert len(lora_layers) == 160
+    # assert len(lora_layers) == 192 # TODO(ryand): Enable this check once conv layers are added.
     for layer_name in lora_layers._names:
-        assert layer_name.endswith(("to_q", "to_k", "to_v", "to_out.0"))
+        assert layer_name.endswith(("to_q", "to_k", "to_v", "to_out.0", "ff.net.0.proj", "ff.net.2"))
+
+
+@pytest.mark.loads_model
+def test_convert_lora_state_dict_to_kohya_format_sd1_smoke():
+    """Smoke test of convert_lora_state_dict_to_kohya_format_sd1(...) with full SD 1.5 model."""
+    unet = UNet2DConditionModel.from_pretrained(
+        "runwayml/stable-diffusion-v1-5", subfolder="unet", local_files_only=True
+    )
+
+    lora_layers = inject_lora_into_unet_sd1(unet)
+    lora_state_dict = lora_layers.get_lora_state_dict()
+    kohya_state_dict = convert_lora_state_dict_to_kohya_format_sd1(lora_state_dict)
+
+    # These assertions are based on a manual check of the injected layers and comparison against the behaviour of
+    # kohya_ss. They are included here to force another manual review after any future breaking change.
+    assert len(kohya_state_dict) == 160 * 3
+    for key in kohya_state_dict.keys():
+        assert key.startswith("lora_unet_")
+        assert key.endswith((".lora_down.weight", ".lora_up.weight", ".alpha"))
+
+
+def test_convert_lora_state_dict_to_kohya_format_sd1():
+    """Basic test of convert_lora_state_dict_to_kohya_format_sd1(...)."""
+    down_weight = torch.Tensor(4, 2)
+    up_weight = torch.Tensor(2, 4)
+    alpha = torch.Tensor([1.0])
+    in_state_dict = {
+        "lora_unet.down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q._down.weight": down_weight,
+        "lora_unet.down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q._up.weight": up_weight,
+        "lora_unet.down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q.alpha": alpha,
+    }
+
+    out_state_dict = convert_lora_state_dict_to_kohya_format_sd1(in_state_dict)
+
+    expected_out_state_dict = {
+        "lora_unet_down_blocks_0_attentions_0_transformer_blocks_0_attn1_to_q.lora_down.weight": down_weight,
+        "lora_unet_down_blocks_0_attentions_0_transformer_blocks_0_attn1_to_q.lora_up.weight": up_weight,
+        "lora_unet_down_blocks_0_attentions_0_transformer_blocks_0_attn1_to_q.alpha": alpha,
+    }
+
+    assert out_state_dict == expected_out_state_dict
+
+
+def test_convert_lora_state_dict_to_kohya_format_sd1_unexpected_key():
+    """Test that convert_lora_state_dict_to_kohya_format_sd1(...) raises an exception if it receives an unexpected
+    key.
+    """
+    in_state_dict = {
+        "lora_unet.down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q._down.unexpected": torch.Tensor(4, 2),
+    }
+
+    with pytest.raises(ValueError):
+        _ = convert_lora_state_dict_to_kohya_format_sd1(in_state_dict)
+
+
+def test_convert_lora_state_dict_to_kohya_format_sd1_conflicting_keys():
+    """Test that convert_lora_state_dict_to_kohya_format_sd1(...) raises an exception if multiple keys map to the same
+    output key.
+    """
+    # Note: There are differences in the '.' and '_' characters of these keys, but they both map to the same output
+    # kohya_ss keys.
+    in_state_dict = {
+        "lora_unet.down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q._down.weight": torch.Tensor(4, 2),
+        "lora_unet.down_blocks.0.attentions.0.transformer_blocks.0.attn1_to_q._down.weight": torch.Tensor(4, 2),
+    }
+
+    with pytest.raises(RuntimeError):
+        _ = convert_lora_state_dict_to_kohya_format_sd1(in_state_dict)
