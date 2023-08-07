2289688a80ab9b2e8342f0b0e59945d77f79cfb7
Ryan Dick <ryanjdick3@gmail.com>
2023-08-07 18:44:59 -0400

    Add HFDirImageCaptionReader.

---
+++ src/invoke_training/training/shared/datasets/hf_dir_image_caption_reader.py
@@ -0,0 +1,81 @@
+import os
+import typing
+
+import datasets
+
+from invoke_training.training.shared.datasets.base_image_caption_reader import (
+    BaseImageCaptionReader,
+)
+
+
+class HFDirImageCaptionReader(BaseImageCaptionReader):
+    """An image-caption dataset reader for datasets in the Hugging Face Datasets Imagefolder format
+    (https://huggingface.co/docs/datasets/v2.4.0/en/image_load#imagefolder).
+    """
+
+    def __init__(
+        self,
+        dataset_dir: str,
+        hf_load_dataset_kwargs: typing.Optional[dict[str, typing.Any]] = None,
+        image_column: str = "image",
+        caption_column: str = "text",
+    ):
+        """Initialize a HFDirImageCaptionReader.
+
+        Args:
+            dataset_dir (str): The path to the dataset directory.
+            hf_load_dataset_kwargs (dict[str, typing.Any], optional): kwargs to forward to `datasets.load_dataset(...)`.
+            image_column (str, optional): The name of the image column in the dataset. Defaults to "image".
+            caption_column (str, optional): The name of the caption column in the dataset. Defaults to "text".
+
+        Raises:
+            ValueError: If `image_column` is not found in the dataset.
+            ValueError: If `caption_column` is not found in the dataset.
+        """
+        super().__init__()
+        hf_load_dataset_kwargs = hf_load_dataset_kwargs or {}
+        data_files = {"train": os.path.join(dataset_dir, "**")}
+        # See more about loading custom images at
+        # https://huggingface.co/docs/datasets/v2.4.0/en/image_load#imagefolder
+        hf_dataset = datasets.load_dataset("imagefolder", data_files=data_files, **hf_load_dataset_kwargs)
+
+        column_names = hf_dataset["train"].column_names
+        if image_column not in column_names:
+            raise ValueError(
+                f"The image_column='{image_column}' is not in the set of dataset column names: '{column_names}'."
+            )
+
+        if caption_column not in column_names:
+            raise ValueError(
+                f"The caption_column='{caption_column}' is not in the set of dataset column names: '{column_names}'."
+            )
+
+        def preprocess(examples):
+            images = [image.convert("RGB") for image in examples[image_column]]
+            return {
+                "image": images,
+                "caption": examples[caption_column],
+            }
+
+        self._hf_dataset = hf_dataset["train"].with_transform(preprocess)
+
+    def __len__(self) -> int:
+        """Get the dataset length.
+
+        Returns:
+            int: The number of image-caption pairs in the dataset.
+        """
+        return len(self._hf_dataset)
+
+    def __getitem__(self, idx: int) -> typing.Dict[str, typing.Any]:
+        """Load the dataset example at index `idx`.
+
+        Raises:
+            IndexError: If `idx` is out of range.
+
+        Returns:
+            dict: A dataset example with 2 keys: "image", and "caption".
+                The "image" key maps to a `PIL` image in RGB format.
+                The "caption" key maps to a string.
+        """
+        return self._hf_dataset[idx]
---
+++ tests/invoke_training/training/shared/datasets/test_hf_dir_image_caption_reader.py
@@ -0,0 +1,97 @@
+import json
+from pathlib import Path
+
+import numpy as np
+import PIL
+import pytest
+from PIL import Image
+
+from invoke_training.training.shared.datasets.hf_dir_image_caption_reader import (
+    HFDirImageCaptionReader,
+)
+
+
+def create_hf_imagefolder_dataset(tmp_dir: Path, num_images: int):
+    """Construct a mock Hugging Face imagefolder dataset in a temporary directory.
+
+    Args:
+        tmp_dir (Path): The temporary directory where the mock dataset will be created.
+        num_images (int): The number of mock images to include in the dataset.
+    """
+    # Construct mock images and save them to disk.
+    rel_img_paths = []
+    for i in range(num_images):
+        rgb_np = np.ones((128, 128, 3), dtype=np.uint8)
+        rgb_pil = Image.fromarray(rgb_np)
+        rel_img_path = f"{i}.jpg"
+        rel_img_paths.append(rel_img_path)
+        rgb_pil.save(tmp_dir / rel_img_path)
+
+    # Construct a mock metadata dict.
+    metadata = []
+    for rel_img_path in rel_img_paths:
+        metadata.append({"file_name": rel_img_path, "text": f"Caption for {rel_img_path}"})
+
+    # Write the metadata.jsonl to disk.
+    metadata_path = tmp_dir / "metadata.jsonl"
+    with open(metadata_path, "w") as f:
+        for metadata_line in metadata:
+            f.write(json.dumps(metadata_line) + "\n")
+
+
+@pytest.fixture(scope="session")
+def hf_imagefolder_dir(tmp_path_factory: pytest.TempPathFactory):
+    """A fixture that prepares a temp directory with a mock Hugging Face imagefolder dataset and returns the directory
+    path.
+
+    Note that the 'session' scope is used to share the same directory across all tests in a session, because it is
+    costly to populate the directory.
+
+    Refer to https://docs.pytest.org/en/7.4.x/how-to/tmp_path.html#the-tmp-path-factory-fixture for details on the use
+    of tmp_path_factory.
+    """
+    tmp_dir = tmp_path_factory.mktemp("dataset")
+    create_hf_imagefolder_dataset(tmp_dir, 5)
+    return tmp_dir
+
+
+@pytest.fixture
+def hf_dir_dataset(hf_imagefolder_dir: Path):
+    return HFDirImageCaptionReader(str(hf_imagefolder_dir))
+
+
+def test_hf_dir_image_caption_reader_bad_image_column(hf_imagefolder_dir: Path):
+    """Test that a ValueError is raised if HFDirImageCaptionReader is initialized with an `image_column` that does not
+    exist.
+    """
+    with pytest.raises(ValueError):
+        _ = HFDirImageCaptionReader(str(hf_imagefolder_dir), image_column="does_not_exist")
+
+
+def test_hf_dir_image_caption_reader_bad_caption_column(hf_imagefolder_dir: Path):
+    """Test that a ValueError is raised if HFDirImageCaptionReader is initialized with a `caption_column` that does not
+    exist.
+    """
+    with pytest.raises(ValueError):
+        _ = HFDirImageCaptionReader(str(hf_imagefolder_dir), caption_column="does_not_exist")
+
+
+def test_hf_dir_image_caption_reader_len(hf_dir_dataset: HFDirImageCaptionReader):
+    """Test the behaviour of HFDirImageCaptionReader.__len__()."""
+    assert len(hf_dir_dataset) == 5
+
+
+def test_hf_dir_image_caption_reader_index_error(hf_dir_dataset: HFDirImageCaptionReader):
+    """Test that an IndexError is raised if a dataset element is accessed with an index that is out-of-bounds."""
+    with pytest.raises(IndexError):
+        _ = hf_dir_dataset[1000]
+
+
+def test_hf_dir_image_caption_reader_getitem(hf_dir_dataset: HFDirImageCaptionReader):
+    """Test that HFDirImageCaptionReader.__getitem__(...) returns a valid example."""
+    example = hf_dir_dataset[0]
+
+    assert set(example.keys()) == {"image", "caption"}
+    assert isinstance(example["image"], PIL.Image.Image)
+    assert example["image"].mode == "RGB"
+    assert isinstance(example["caption"], str)
