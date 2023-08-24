507bf39e8e59f91e83d56a0cb4a1a39299782486
Ryan Dick <ryanjdick3@gmail.com>
2023-08-24 08:58:53 -0400

    Add more data types to TensorDiskCache unit test.

--- tests/invoke_training/training/shared/data/transforms/test_tensor_disk_cache.py
+++ tests/invoke_training/training/shared/data/transforms/test_tensor_disk_cache.py
@@ -12,15 +12,17 @@ def test_tensor_disk_cache_roundtrip(tmp_path: Path):
     """Test a TensorDiskCache cache roundtrip."""
     cache = TensorDiskCache(str(tmp_path))
 
-    in_dict = {"test_tensor": torch.rand((1, 2, 3))}
+    in_dict = {"test_tensor": torch.rand((1, 2, 3)), "test_tuple": (1, 2), "test_list": [3, 4], "test_scalar": 1}
 
     # Roundtrip
     cache.save(0, in_dict)
     out_dict = cache.load(0)
 
     assert set(in_dict.keys()) == set(out_dict.keys())
-    for key in in_dict.keys():
-        torch.testing.assert_close(out_dict[key], in_dict[key])
+    torch.testing.assert_close(out_dict["test_tensor"], in_dict["test_tensor"])
+    assert out_dict["test_tuple"] == in_dict["test_tuple"]
+    assert out_dict["test_list"] == in_dict["test_list"]
+    assert out_dict["test_scalar"] == in_dict["test_scalar"]
 
 
 def test_tensor_disk_cache_fail_overwrite(tmp_path):
