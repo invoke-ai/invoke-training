3bd340ffd473fe8a944f29b955c59c5d4e7a6a03
Ryan Dick <ryanjdick3@gmail.com>
2023-08-30 19:49:21 -0400

    Add invoke-generate-images script.

---
+++ src/invoke_training/training/tools/generate_images.py
@@ -0,0 +1,63 @@
+import os
+
+import torch
+from diffusers import DiffusionPipeline
+from tqdm import tqdm
+
+
+def generate_images(
+    out_dir: str,
+    model: str,
+    prompt: str,
+    num_images: int,
+    height: int,
+    width: int,
+    seed: int = 0,
+    torch_dtype: torch.dtype = torch.float16,
+    torch_device: str = "cuda",
+    enable_cpu_offload: bool = False,
+):
+    """Generate a set of images and store them in a directory. Typically used to generate a datasets for prior
+    preservation / regularization.
+
+    Args:
+        out_dir (str): The output directory to create.
+        model (str): The name or path of the diffusers pipeline to generate with.
+        prompt (str): The prompt to generate images with.
+        num_images (int): The number of images to generate.
+        height (int): The output image height in pixels (recommended to match the resolution that the model was trained
+            with).
+        width (int): The output image width in pixels (recommended to match the resolution that the model was trained
+            with).
+        seed (int, optional): A seed for repeatability. Defaults to 0.
+        torch_dtype (torch.dtype, optional): The torch dtype. Defaults to torch.float16.
+        torch_device (str, optional): The torch device. Defaults to "cuda".
+        enable_cpu_offload (bool, optional): If True, models will be loaded onto the GPU one by one to conserve VRAM.
+            Defaults to False.
+    """
+    pipeline = DiffusionPipeline.from_pretrained(model)
+
+    pipeline.to(torch_dtype=torch_dtype)
+    if enable_cpu_offload:
+        pipeline.enable_model_cpu_offload()
+    else:
+        pipeline.to(torch_device=torch_device)
+    pipeline.set_progress_bar_config(disable=True)
+
+    generator = torch.Generator(device=torch_device)
+    if seed is not None:
+        generator = generator.manual_seed(seed)
+
+    os.makedirs(out_dir)
+
+    with torch.no_grad():
+        for i in tqdm(range(num_images)):
+            image = pipeline(
+                prompt,
+                num_inference_steps=30,
+                generator=generator,
+                height=height,
+                width=width,
+            ).images[0]
+
+            image.save(os.path.join(out_dir, f"{i:0>4}.jpg"))
