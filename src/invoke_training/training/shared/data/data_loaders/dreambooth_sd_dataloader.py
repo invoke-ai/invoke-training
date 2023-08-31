947be517d4eb4916555741689bbf571daaca6537
Ryan Dick <ryanjdick3@gmail.com>
2023-08-31 15:09:43 -0400

    Implement build_dreambooth_sd_dataloader(...).

--- src/invoke_training/training/shared/data/data_loaders/dreambooth_sd_dataloader.py
+++ src/invoke_training/training/shared/data/data_loaders/dreambooth_sd_dataloader.py
@@ -1,7 +1,25 @@
 import typing
 
 import torch
-from torch.utils.data import Sampler
+from torch.utils.data import ConcatDataset, DataLoader, Sampler
+from transformers import CLIPTokenizer
+
+from invoke_training.training.config.data_config import ImageDirDatasetConfig
+from invoke_training.training.shared.data.datasets.image_dir_dataset import (
+    ImageDirDataset,
+)
+from invoke_training.training.shared.data.datasets.transform_dataset import (
+    TransformDataset,
+)
+from invoke_training.training.shared.data.transforms.constant_field_transform import (
+    ConstantFieldTransform,
+)
+from invoke_training.training.shared.data.transforms.sd_image_transform import (
+    SDImageTransform,
+)
+from invoke_training.training.shared.data.transforms.sd_tokenize_transform import (
+    SDTokenizeTransform,
+)
 
 
 class SequentialRangeSampler(Sampler[int]):
@@ -73,3 +91,67 @@ class InterleavedSampler(Sampler[int]):
 
     def __len__(self) -> int:
         return self._min_sampler_len * len(self._samplers)
+
+
+def build_dreambooth_sd_dataloader(
+    instance_prompt: str,
+    instance_dataset_config: ImageDirDatasetConfig,
+    class_prompt: typing.Optional[str],
+    class_data_dir: typing.Optional[str],
+    tokenizer: typing.Optional[CLIPTokenizer],
+    batch_size: int,
+    shuffle: bool = True,
+) -> DataLoader:
+    """Construct a DataLoader for a DreamBooth dataset for Stable Diffusion v1/v2.."""
+
+    # 1. Prepare instance dataset
+    instance_dataset = ImageDirDataset(instance_dataset_config.dataset_dir)
+    instance_dataset = TransformDataset(
+        instance_dataset,
+        [ConstantFieldTransform("caption", instance_prompt), ConstantFieldTransform("loss_weight", 1.0)],
+    )
+
+    # 2. Prepare class dataset.
+    class_dataset = None
+    if class_data_dir is not None:
+        class_dataset = ImageDirDataset(class_data_dir)
+        class_dataset = TransformDataset(
+            class_dataset,
+            [ConstantFieldTransform("caption", class_prompt), ConstantFieldTransform("loss_weight", 1.0)],
+        )
+
+    # 3. Merge instance dataset and class dataset.
+    merged_dataset = ConcatDataset([instance_dataset, class_dataset])
+    all_transforms = [
+        SDImageTransform(
+            resolution=instance_dataset_config.image_transforms.resolution,
+            center_crop=instance_dataset_config.image_transforms.center_crop,
+            random_flip=instance_dataset_config.image_transforms.random_flip,
+        ),
+        SDTokenizeTransform(tokenizer),
+    ]
+    merged_dataset = TransformDataset(merged_dataset, all_transforms)
+
+    # 4. Prepare instance dataset sampler. Note that the instance_dataset comes first in the merged_dataset.
+    samplers = []
+    if shuffle:
+        samplers.append(SequentialRangeSampler(0, len(instance_dataset)))
+    else:
+        samplers.append(ShuffledRangeSampler(0, len(instance_dataset)))
+
+    # 5. Prepare class dataset sampler. Note that the class_dataset comes first in the merged_dataset.
+    if class_dataset is not None:
+        if shuffle:
+            samplers.append(SequentialRangeSampler(len(instance_dataset), len(instance_dataset) + len(class_dataset)))
+        else:
+            samplers.append(ShuffledRangeSampler(len(instance_dataset), len(instance_dataset) + len(class_dataset)))
+
+    # 6. Interleave instance and class samplers.
+    interleaved_sampler = InterleavedSampler(samplers)
+
+    return DataLoader(
+        merged_dataset,
+        sampler=interleaved_sampler,
+        batch_size=batch_size,
+        num_workers=instance_dataset_config.dataloader_num_workers,
+    )
--- tests/invoke_training/training/shared/data/data_loaders/test_dreambooth_sd_dataloader.py
+++ tests/invoke_training/training/shared/data/data_loaders/test_dreambooth_sd_dataloader.py
@@ -1,10 +1,16 @@
+import pytest
 import torch
+from transformers import CLIPTokenizer
 
+from invoke_training.training.config.data_config import ImageDirDatasetConfig
 from invoke_training.training.shared.data.data_loaders.dreambooth_sd_dataloader import (
     InterleavedSampler,
     SequentialRangeSampler,
     ShuffledRangeSampler,
+    build_dreambooth_sd_dataloader,
 )
+
+from ..image_dir_fixture import image_dir  # noqa: F401
 
 #########################
 # SequentialRangeSampler
@@ -120,3 +126,49 @@ def test_interleaved_sampler_len():
 
     sampler = InterleavedSampler([sampler_1, sampler_2, sampler_3])
     assert len(sampler) == 2 * 3
+
+
+#################################
+# build_dreambooth_sd_dataloader
+#################################
+
+
+@pytest.mark.loads_model
+def test_build_dreambooth_sd_dataloader(image_dir):  # noqa: F811
+    """Smoke test of build_dreambooth_sd_dataloader(...)."""
+
+    tokenizer = CLIPTokenizer.from_pretrained(
+        "runwayml/stable-diffusion-v1-5",
+        subfolder="tokenizer",
+        local_files_only=True,
+        revision="c9ab35ff5f2c362e9e22fbafe278077e196057f0",
+    )
+
+    config = ImageDirDatasetConfig(dataset_dir=str(image_dir))
+
+    data_loader = build_dreambooth_sd_dataloader(
+        instance_prompt="test instance prompt",
+        instance_dataset_config=config,
+        class_prompt="test class prompt",
+        # For testing, we just use the same directory for the instance and class datasets.
+        class_data_dir=str(image_dir),
+        tokenizer=tokenizer,
+        batch_size=2,
+    )
+
+    assert len(data_loader) == 5  # (5 class images + 5 instance images) / batch size 2
+
+    example = next(iter(data_loader))
+    assert set(example.keys()) == {"image", "caption", "id", "caption_token_ids", "loss_weight"}
+
+    image = example["image"]
+    assert image.shape == (2, 3, 512, 512)
+    assert image.dtype == torch.float32
+
+    caption_token_ids = example["caption_token_ids"]
+    assert caption_token_ids.shape == (2, 77)
+    assert caption_token_ids.dtype == torch.int64
+
+    loss_weight = example["loss_weight"]
+    assert loss_weight.shape == (2,)
+    assert loss_weight.dtype == torch.float64
