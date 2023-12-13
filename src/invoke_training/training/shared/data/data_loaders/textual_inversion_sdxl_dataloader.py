ae3684ff35d448cec56a3f755eb2388cadf1727a
Ryan Dick <ryanjdick3@gmail.com>
2023-12-13 10:02:14 -0500

    Add TI SDXL dataloader.

--- src/invoke_training/training/shared/data/data_loaders/textual_inversion_sdxl_dataloader.py
+++ src/invoke_training/training/shared/data/data_loaders/textual_inversion_sdxl_dataloader.py
@@ -1,17 +1,23 @@
-import typing
+from typing import Optional
 
 from torch.utils.data import DataLoader
 
-from invoke_training.config.shared.data.data_loader_config import TextualInversionSDDataLoaderConfig
+from invoke_training.config.shared.data.data_loader_config import TextualInversionSDXLDataLoaderConfig
 from invoke_training.config.shared.data.transform_config import (
     TextualInversionCaptionTransformConfig,
     TextualInversionPresetCaptionTransformConfig,
+)
+from invoke_training.training.shared.data.data_loaders.image_caption_sdxl_dataloader import (
+    sdxl_image_caption_collate_fn,
+)
+from invoke_training.training.shared.data.data_loaders.textual_inversion_sd_dataloader import (
+    get_preset_ti_caption_templates,
 )
 from invoke_training.training.shared.data.datasets.image_dir_dataset import ImageDirDataset
 from invoke_training.training.shared.data.datasets.transform_dataset import TransformDataset
 from invoke_training.training.shared.data.transforms.drop_field_transform import DropFieldTransform
 from invoke_training.training.shared.data.transforms.load_cache_transform import LoadCacheTransform
-from invoke_training.training.shared.data.transforms.sd_image_transform import SDImageTransform
+from invoke_training.training.shared.data.transforms.sdxl_image_transform import SDXLImageTransform
 from invoke_training.training.shared.data.transforms.shuffle_caption_transform import ShuffleCaptionTransform
 from invoke_training.training.shared.data.transforms.tensor_disk_cache import TensorDiskCache
 from invoke_training.training.shared.data.transforms.textual_inversion_caption_transform import (
@@ -19,68 +25,11 @@ from invoke_training.training.shared.data.transforms.textual_inversion_caption_t
 )
 
 
-def get_preset_ti_caption_templates(preset: typing.Literal["object", "style"]) -> list[str]:
-    if preset == "object":
-        return [
-            "a photo of a {}",
-            "a rendering of a {}",
-            "a cropped photo of the {}",
-            "the photo of a {}",
-            "a photo of a clean {}",
-            "a photo of a dirty {}",
-            "a dark photo of the {}",
-            "a photo of my {}",
-            "a photo of the cool {}",
-            "a close-up photo of a {}",
-            "a bright photo of the {}",
-            "a cropped photo of a {}",
-            "a photo of the {}",
-            "a good photo of the {}",
-            "a photo of one {}",
-            "a close-up photo of the {}",
-            "a rendition of the {}",
-            "a photo of the clean {}",
-            "a rendition of a {}",
-            "a photo of a nice {}",
-            "a good photo of a {}",
-            "a photo of the nice {}",
-            "a photo of the small {}",
-            "a photo of the weird {}",
-            "a photo of the large {}",
-            "a photo of a cool {}",
-            "a photo of a small {}",
-        ]
-    elif preset == "style":
-        return [
-            "a painting in the style of {}",
-            "a rendering in the style of {}",
-            "a cropped painting in the style of {}",
-            "the painting in the style of {}",
-            "a clean painting in the style of {}",
-            "a dirty painting in the style of {}",
-            "a dark painting in the style of {}",
-            "a picture in the style of {}",
-            "a cool painting in the style of {}",
-            "a close-up painting in the style of {}",
-            "a bright painting in the style of {}",
-            "a cropped painting in the style of {}",
-            "a good painting in the style of {}",
-            "a close-up painting in the style of {}",
-            "a rendition in the style of {}",
-            "a nice painting in the style of {}",
-            "a small painting in the style of {}",
-            "a weird painting in the style of {}",
-            "a large painting in the style of {}",
-        ]
-    else:
-        raise ValueError(f"Unrecognized learnable property type: '{preset}'.")
-
-
-def build_textual_inversion_sd_dataloader(
-    config: TextualInversionSDDataLoaderConfig,
+def build_textual_inversion_sdxl_dataloader(
+    config: TextualInversionSDXLDataLoaderConfig,
     placeholder_str: str,
     batch_size: int,
-    vae_output_cache_dir: typing.Optional[str] = None,
+    vae_output_cache_dir: Optional[str] = None,
     shuffle: bool = True,
 ) -> DataLoader:
     """Construct a DataLoader for a Textual Inversion dataset for Stable Diffusion v1/v2..
@@ -122,7 +71,7 @@ def build_textual_inversion_sd_dataloader(
 
     if vae_output_cache_dir is None:
         all_transforms.append(
-            SDImageTransform(
+            SDXLImageTransform(
                 resolution=config.image_transforms.resolution,
                 center_crop=config.image_transforms.center_crop,
                 random_flip=config.image_transforms.random_flip,
@@ -132,7 +81,13 @@ def build_textual_inversion_sd_dataloader(
         vae_cache = TensorDiskCache(vae_output_cache_dir)
         all_transforms.append(
             LoadCacheTransform(
-                cache=vae_cache, cache_key_field="id", cache_field_to_output_field={"vae_output": "vae_output"}
+                cache=vae_cache,
+                cache_key_field="id",
+                cache_field_to_output_field={
+                    "vae_output": "vae_output",
+                    "original_size_hw": "original_size_hw",
+                    "crop_top_left_yx": "crop_top_left_yx",
+                },
             )
         )
         # We drop the image to avoid having to either convert from PIL, or handle PIL batch collation.
@@ -143,6 +98,7 @@ def build_textual_inversion_sd_dataloader(
     return DataLoader(
         dataset,
         shuffle=shuffle,
+        collate_fn=sdxl_image_caption_collate_fn,
         batch_size=batch_size,
         num_workers=config.dataloader_num_workers,
     )
