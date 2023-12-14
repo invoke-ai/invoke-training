3cffd50b07d6f9de78b695a8b72324be8d646b19
Ryan Dick <ryanjdick3@gmail.com>
2023-12-14 09:22:12 -0500

    Add TI SDXL support to invoke-train command.

--- src/invoke_training/config/pipelines/pipeline_config.py
+++ src/invoke_training/config/pipelines/pipeline_config.py
@@ -6,13 +6,17 @@ from invoke_training.config.pipelines.finetune_lora_config import (
     FinetuneLoRASDConfig,
     FinetuneLoRASDXLConfig,
 )
-from invoke_training.config.pipelines.textual_inversion_config import TextualInversionSDConfig
+from invoke_training.config.pipelines.textual_inversion_config import (
+    TextualInversionSDConfig,
+    TextualInversionSDXLConfig,
+)
 
 PipelineConfig = Annotated[
     Union[
         FinetuneLoRASDConfig,
         FinetuneLoRASDXLConfig,
         TextualInversionSDConfig,
+        TextualInversionSDXLConfig,
     ],
     Field(discriminator="type"),
 ]
