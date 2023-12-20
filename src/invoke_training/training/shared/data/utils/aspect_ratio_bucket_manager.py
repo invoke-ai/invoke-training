7b95f517338f371ae2865eb53ab73ba71df5a1b9
Ryan Dick <ryanjdick3@gmail.com>
2023-12-19 19:32:39 -0500

    Implement AspectRatioBucketManager.get_aspect_ratio_bucket(...).

--- src/invoke_training/training/shared/data/utils/aspect_ratio_bucket_manager.py
+++ src/invoke_training/training/shared/data/utils/aspect_ratio_bucket_manager.py
@@ -1,20 +1,37 @@
+class Resolution:
+    def __init__(self, height: int, width: int):
+        self.height = height
+        self.width = width
+
+    def aspect_ratio(self):
+        return self.height / self.width
+
+    def __eq__(self, other: "Resolution") -> bool:
+        return self.height == other.height and self.width == other.width
+
+    def __hash__(self):
+        return hash((self.height, self.width))
+
+
 class AspectRatioBucketManager:
-    def __init__(self, target_resolution: tuple[int, int], start_dim: int, end_dim: int, divisible_by: int) -> None:
-        self._buckets = self.build_aspect_ratio_buckets(
-            target_resolution=target_resolution,
-            start_dim=start_dim,
-            end_dim=end_dim,
-            divisible_by=divisible_by,
+    def __init__(self, target_resolution: Resolution, start_dim: int, end_dim: int, divisible_by: int) -> None:
+        self._buckets = list(
+            self.build_aspect_ratio_buckets(
+                target_resolution=target_resolution,
+                start_dim=start_dim,
+                end_dim=end_dim,
+                divisible_by=divisible_by,
+            )
         )
 
     @classmethod
     def build_aspect_ratio_buckets(
-        cls, target_resolution: tuple[int, int], start_dim: int, end_dim: int, divisible_by: int
-    ) -> set[tuple[int, int]]:
+        cls, target_resolution: Resolution, start_dim: int, end_dim: int, divisible_by: int
+    ) -> set[Resolution]:
         """Prepare a set of aspect ratios.
 
         Args:
-            target_resolution (tuple[int, int]): The target resolution. All resolutions in the returned set will have <=
+            target_resolution (Resolution): The target resolution. All resolutions in the returned set will have <=
                 the number of pixels in this target resolution.
             start_dim (int):
             end_dim (int):
@@ -24,25 +41,29 @@ class AspectRatioBucketManager:
             set[tuple[int, int]]: The aspect ratio bucket resolutions.
         """
         # Validate target_resolution.
-        target_res_h, target_res_w = target_resolution
-        assert target_res_h % divisible_by == 0
-        assert target_res_w % divisible_by == 0
+        assert target_resolution.height % divisible_by == 0
+        assert target_resolution.width % divisible_by == 0
 
         # Validate start_dim, end_dim.
         assert start_dim <= end_dim
         assert start_dim % divisible_by == 0
         assert end_dim % divisible_by == 0
 
-        target_size = target_res_h * target_res_w
+        target_size = target_resolution.height * target_resolution.width
 
         buckets = set()
 
         height = start_dim
         while height <= end_dim:
             width = (target_size // height) // divisible_by * divisible_by
-            buckets.add((height, width))
-            buckets.add((width, height))
+            buckets.add(Resolution(height, width))
+            buckets.add(Resolution(width, height))
 
             height += divisible_by
 
         return buckets
+
+    def get_aspect_ratio_bucket(self, resolution: Resolution):
+        """Get the bucket with the closest aspect ratio to 'resolution'."""
+        # Note: If this is ever found to be a bottleneck, there is a clearly-more-efficient implementation using bisect.
+        return min(self._buckets, key=lambda x: abs(x.aspect_ratio() - resolution.aspect_ratio()))
--- tests/invoke_training/training/shared/data/utils/test_aspect_ratio_bucket_manager.py
+++ tests/invoke_training/training/shared/data/utils/test_aspect_ratio_bucket_manager.py
@@ -2,26 +2,22 @@ from contextlib import nullcontext
 
 import pytest
 
-from invoke_training.training.shared.data.utils.aspect_ratio_bucket_manager import AspectRatioBucketManager
-
-
-def test_aspect_ratio_bucket_manager():
-    assert False
+from invoke_training.training.shared.data.utils.aspect_ratio_bucket_manager import AspectRatioBucketManager, Resolution
 
 
 @pytest.mark.parametrize(
     ["target_resolution", "start_dim", "end_dim", "divisible_by", "should_raise"],
     [
-        ((1024, 1024), 512, 2048, 64, False),
-        ((1025, 1024), 512, 2048, 64, True),  # target_resolution[0] not divisible by divisible_by.
-        ((1024, 1025), 512, 2048, 64, True),  # target_resolution[1] not divisible by divisible_by.
-        ((1024, 1024), 513, 2048, 64, True),  # start_dim not divisible by divisible_by.
-        ((1024, 1024), 512, 2049, 64, True),  # end_dim not divisible by divisible_by.
-        ((1024, 1024), 1024, 512, 64, True),  # start_dim > end_dim.
+        (Resolution(1024, 1024), 512, 2048, 64, False),
+        (Resolution(1025, 1024), 512, 2048, 64, True),  # target_resolution[0] not divisible by divisible_by.
+        (Resolution(1024, 1025), 512, 2048, 64, True),  # target_resolution[1] not divisible by divisible_by.
+        (Resolution(1024, 1024), 513, 2048, 64, True),  # start_dim not divisible by divisible_by.
+        (Resolution(1024, 1024), 512, 2049, 64, True),  # end_dim not divisible by divisible_by.
+        (Resolution(1024, 1024), 1024, 512, 64, True),  # start_dim > end_dim.
     ],
 )
 def test_build_aspect_ratio_buckets_input_validation(
-    target_resolution: tuple[int, int], start_dim: int, end_dim: int, divisible_by: int, should_raise: bool
+    target_resolution: Resolution, start_dim: int, end_dim: int, divisible_by: int, should_raise: bool
 ):
     """Test validation of all input params to AspectRatioBucketManager.build_aspect_ratio_buckets(...)."""
     expectation = pytest.raises(AssertionError) if should_raise else nullcontext()
@@ -38,17 +34,29 @@ def test_build_aspect_ratio_buckets_input_validation(
     ["target_resolution", "start_dim", "end_dim", "divisible_by", "expected"],
     [
         # 1 bucket
-        ((1024, 1024), 1024, 1024, 64, {(1024, 1024)}),
+        (Resolution(1024, 1024), 1024, 1024, 64, {Resolution(1024, 1024)}),
         # Multiple buckets.
-        ((1024, 1024), 768, 1280, 128, {(768, 1280), (896, 1152), (1024, 1024), (1152, 896), (1280, 768)}),
+        (
+            Resolution(1024, 1024),
+            768,
+            1280,
+            128,
+            {
+                Resolution(768, 1280),
+                Resolution(896, 1152),
+                Resolution(1024, 1024),
+                Resolution(1152, 896),
+                Resolution(1280, 768),
+            },
+        ),
     ],
 )
 def test_build_aspect_ratio_buckets(
-    target_resolution: tuple[int, int],
+    target_resolution: Resolution,
     start_dim: int,
     end_dim: int,
     divisible_by: int,
-    expected: set[tuple[int, int]],
+    expected: set[Resolution],
 ):
     buckets = AspectRatioBucketManager.build_aspect_ratio_buckets(
         target_resolution=target_resolution,
@@ -58,3 +66,21 @@ def test_build_aspect_ratio_buckets(
     )
 
     assert buckets == expected
+
+
+@pytest.mark.parametrize(
+    ["resolution", "expected_bucket"],
+    [
+        (Resolution(1024, 1024), Resolution(1024, 1024)),  # Exact match.
+        (Resolution(128, 1024), Resolution(768, 1280)),  # Small aspect ratio.
+        (Resolution(1024, 128), Resolution(1280, 768)),  # Large aspect ratio.
+    ],
+)
+def test_get_aspect_ratio_bucket(resolution: Resolution, expected_bucket: Resolution):
+    arbm = AspectRatioBucketManager(
+        target_resolution=Resolution(1024, 1024), start_dim=768, end_dim=1280, divisible_by=128
+    )
+
+    nearest_bucket = arbm.get_aspect_ratio_bucket(resolution)
+
+    assert nearest_bucket == expected_bucket
