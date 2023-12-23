f94b15a272063aceb3c67da99fbdd0cc746900b1
Ryan Dick <ryanjdick3@gmail.com>
2023-12-23 11:37:17 -0500

    Merge the HFHubImageCaptionDataset and HFDirImageCaptionDataset implementations to reduce duplication.

--- src/invoke_training/training/shared/data/datasets/build_dataset.py
+++ src/invoke_training/training/shared/data/datasets/build_dataset.py
@@ -2,12 +2,11 @@ from invoke_training.config.shared.data.dataset_config import (
     HFDirImageCaptionDatasetConfig,
     HFHubImageCaptionDatasetConfig,
 )
-from invoke_training.training.shared.data.datasets.hf_dir_image_caption_dataset import HFDirImageCaptionDataset
-from invoke_training.training.shared.data.datasets.hf_hub_image_caption_dataset import HFHubImageCaptionDataset
+from invoke_training.training.shared.data.datasets.hf_image_caption_dataset import HFImageCaptionDataset
 
 
-def build_hf_hub_image_caption_dataset(config: HFHubImageCaptionDatasetConfig) -> HFHubImageCaptionDataset:
-    return HFHubImageCaptionDataset(
+def build_hf_hub_image_caption_dataset(config: HFHubImageCaptionDatasetConfig) -> HFImageCaptionDataset:
+    return HFImageCaptionDataset.from_hub(
         dataset_name=config.dataset_name,
         hf_load_dataset_kwargs={
             "name": config.dataset_config_name,
@@ -18,8 +17,8 @@ def build_hf_hub_image_caption_dataset(config: HFHubImageCaptionDatasetConfig) -
     )
 
 
-def build_hf_dir_image_caption_dataset(config: HFDirImageCaptionDatasetConfig) -> HFDirImageCaptionDataset:
-    return HFDirImageCaptionDataset(
+def build_hf_dir_image_caption_dataset(config: HFDirImageCaptionDatasetConfig) -> HFImageCaptionDataset:
+    return HFImageCaptionDataset.from_dir(
         dataset_dir=config.dataset_dir,
         hf_load_dataset_kwargs=None,
         image_column=config.image_column,
