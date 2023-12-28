88818b4f91a347f1bb59f0f280a91a818141f36b
Ryan Dick <ryanjdick3@gmail.com>
2023-12-28 15:53:09 -0500

    Increase the size of the pickapic subset used for training to ~2000 image pairs.

--- src/invoke_training/training/shared/data/datasets/build_dataset.py
+++ src/invoke_training/training/shared/data/datasets/build_dataset.py
@@ -37,10 +37,18 @@ def build_hf_image_pair_preference_dataset(
     # 'yuvalkirstain/pickapic_v2' dataset.
     return HFImagePairPreferenceDataset.from_hub(
         "yuvalkirstain/pickapic_v2",
-        split="validation_unique",
+        split="train",
         hf_load_dataset_kwargs={
             "data_files": {
-                "validation_unique": "data/validation_unique-00000-of-00001-33ead111845fc9c4.parquet",
+                # "validation_unique": "data/validation_unique-00000-of-00001-33ead111845fc9c4.parquet",
+                "train": [
+                    "data/train-00000-of-00645-b66ac786bf6fb553.parquet",
+                    "data/train-00001-of-00645-c7b349dd222d6515.parquet",
+                    "data/train-00002-of-00645-e4f54d615a978deb.parquet",
+                    "data/train-00003-of-00645-2b9d59bac8b433ff.parquet",
+                    "data/train-00004-of-00645-e4964649dc0ea543.parquet",
+                    "data/train-00005-of-00645-45e8efc0fe93f6e9.parquet",
+                ]
             },
             # Disable checks so that it doesn't complain that I haven't downloaded the other splits.
             "verification_mode": VerificationMode.NO_CHECKS,
