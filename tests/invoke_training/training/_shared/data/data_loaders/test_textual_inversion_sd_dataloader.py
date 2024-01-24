faabb252cbc0a1c423dfcc9353199d9c4cd675ca
Ryan Dick <ryanjdick3@gmail.com>
2024-01-24 18:31:01 -0500

    Add keep_original_captions option to TextualInversionSDDataLoaderConfig.

--- tests/invoke_training/training/_shared/data/data_loaders/test_textual_inversion_sd_dataloader.py
+++ tests/invoke_training/training/_shared/data/data_loaders/test_textual_inversion_sd_dataloader.py
@@ -1,7 +1,7 @@
 import torch
 
 from invoke_training.config.shared.data.data_loader_config import TextualInversionSDDataLoaderConfig
-from invoke_training.config.shared.data.dataset_config import ImageDirDatasetConfig
+from invoke_training.config.shared.data.dataset_config import HFHubImageCaptionDatasetConfig, ImageDirDatasetConfig
 from invoke_training.training._shared.data.data_loaders.textual_inversion_sd_dataloader import (
     build_textual_inversion_sd_dataloader,
 )
@@ -42,3 +42,25 @@ def test_build_textual_inversion_sd_dataloader(image_dir):  # noqa: F811
     crop_top_left_yx = example["crop_top_left_yx"]
     assert len(crop_top_left_yx) == 2
     assert len(crop_top_left_yx[0]) == 2
+
+
+def test_build_textual_inversion_sd_dataloader_keep_original_captions():
+    """Test the keep_original_captions=True option."""
+    config = TextualInversionSDDataLoaderConfig(
+        dataset=HFHubImageCaptionDatasetConfig(dataset_name="lambdalabs/pokemon-blip-captions"),
+        caption_templates=["{}"],
+        keep_original_captions=True,
+    )
+
+    data_loader = build_textual_inversion_sd_dataloader(
+        config=config,
+        placeholder_token="placeholder",
+        batch_size=2,
+    )
+
+    example = next(iter(data_loader))
+    assert set(example.keys()) == {"image", "id", "caption", "original_size_hw", "crop_top_left_yx"}
+
+    assert len(example["caption"]) == 2
+    for caption in example["caption"]:
+        assert caption.startswith("placeholder ")
