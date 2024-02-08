cf57a7595d2ae4feb942b23171f106bd556d7cfa
Ryan Dick <ryanjdick3@gmail.com>
2024-02-08 11:10:38 -0500

    Add UI section for loading and creating a dataset for editing.

--- src/invoke_training/_shared/data/datasets/image_caption_jsonl_dataset.py
+++ src/invoke_training/_shared/data/datasets/image_caption_jsonl_dataset.py
@@ -3,36 +3,34 @@ from pathlib import Path
 
 import torch.utils.data
 from PIL import Image
+from pydantic import BaseModel
 
 from invoke_training._shared.data.utils.resolution import Resolution
 from invoke_training._shared.utils.jsonl import load_jsonl
 
 
+class _ImageCaptionExample(BaseModel):
+    image_path: str
+    caption: str
+
+
 class ImageCaptionJsonlDataset(torch.utils.data.Dataset):
     """A dataset that loads images and captions from a directory of image files and .txt files."""
 
-    def __init__(
-        self,
-        jsonl_path: str,
-        image_column: str = "image",
-        caption_column: str = "text",
-    ):
-        """Initialize an ImageCaptionDirDataset"""
+    def __init__(self, jsonl_path: Path, image_column: str = "image", caption_column: str = "text"):
         super().__init__()
-
-        self._jsonl_path = Path(jsonl_path)
-        self._data: list[dict[str, typing.Any]] = []
-        data = load_jsonl(self._jsonl_path)
-        for d in data:
-            assert image_column in d
-            assert caption_column in d
-        self._data = data
-
+        self._jsonl_path = jsonl_path
         self._image_column = image_column
         self._caption_column = caption_column
 
+        data = load_jsonl(jsonl_path)
+        examples: list[_ImageCaptionExample] = []
+        for d in data:
+            examples.append(_ImageCaptionExample(image_path=d[image_column], caption=d[caption_column]))
+        self._examples = examples
+
     def _get_image_path(self, idx: int) -> str:
-        image_path = self._data[idx][self._image_column]
+        image_path = self._examples[idx].image_path
 
         # image_path could be either absolute, or relative to the jsonl file.
         if not image_path.startswith("/"):
@@ -52,15 +50,15 @@ class ImageCaptionJsonlDataset(torch.utils.data.Dataset):
         calculate this dynamically every time.
         """
         image_dims: list[Resolution] = []
-        for i in range(len(self._data)):
+        for i in range(len(self._examples)):
             image = Image.open(self._get_image_path(i))
             image_dims.append(Resolution(image.height, image.width))
 
         return image_dims
 
     def __len__(self) -> int:
-        return len(self._data)
+        return len(self._examples)
 
     def __getitem__(self, idx: int) -> typing.Dict[str, typing.Any]:
         image = self._load_image(self._get_image_path(idx))
-        return {"id": str(idx), "image": image, "caption": self._data[idx][self._caption_column]}
+        return {"id": str(idx), "image": image, "caption": self._examples[idx].caption}
--- src/invoke_training/ui/pages/data_page.py
+++ src/invoke_training/ui/pages/data_page.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 import gradio as gr
 
+from invoke_training._shared.data.datasets.image_caption_jsonl_dataset import ImageCaptionJsonlDataset
+from invoke_training._shared.utils.jsonl import save_jsonl
 from invoke_training.ui.utils import get_assets_dir_path
 
 
@@ -21,7 +25,54 @@ class DataPage:
                         " Learn more about Invoke at [invoke.com](https://www.invoke.com/)"
                     )
 
-        self._app = app
+            gr.Markdown("# Data Annotation")
+
+            gr.Markdown("To get started, either create a new dataset or load an existing one.")
+            gr.Markdown(
+                "Note: This UI creates datasets in `IMAGE_CAPTION_JSONL_DATASET` format. For more information about "
+                "this format see [the docs](https://invoke-ai.github.io/invoke-training/concepts/dataset_formats/)"
+            )
+
+            gr.Markdown("## Setup")
+            with gr.Group():
+                # TODO: Expose image_column and caption_column as inputs?
+                self._load_path_textbox = gr.Textbox(
+                    label=".jsonl Path",
+                    info="Enter the path to the .jsonl file to load or create.",
+                    placeholder="/path/to/dataset.jsonl",
+                )
+                self._load_dataset_button = gr.Button("Load or Create Dataset")
+
+            gr.Markdown("## Editing ")
+            self._current_jsonl_textbox = gr.Textbox(
+                label="Currently editing", interactive=False, placeholder="No dataset loaded"
+            )
+            self._current_len_number = gr.Number(label="Dataset length", interactive=False)
+
+            self._load_dataset_button.click(
+                self._on_load_dataset_button_click,
+                inputs=set([self._load_path_textbox]),
+                outputs=[self._current_jsonl_textbox, self._current_len_number],
+            )
+
+            self._app = app
+
+    def _on_load_dataset_button_click(self, data: dict):
+        jsonl_path = Path(data[self._load_path_textbox])
+        jsonl_path = jsonl_path.resolve()
+        if jsonl_path.exists():
+            print(f"Loading dataset from '{jsonl_path}'.")
+        else:
+            print(f"Creating new dataset at '{jsonl_path}'.")
+            assert jsonl_path.suffix == ".jsonl"
+            jsonl_path.parent.mkdir(parents=True, exist_ok=True)
+            # Create an empty jsonl file.
+            save_jsonl([], jsonl_path)
+
+        # Initialize the dataset to validate the jsonl file, and to get the length.
+        dataset = ImageCaptionJsonlDataset(jsonl_path)
+
+        return {self._current_jsonl_textbox: jsonl_path, self._current_len_number: len(dataset)}
 
     def app(self):
         return self._app
