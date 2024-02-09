222aebf47187e9d8f19c8f57843fcb1557109172
Ryan Dick <ryanjdick3@gmail.com>
2024-02-09 13:47:19 -0500

    Toggle between showing dataset selection and dataset editing parts of the UI.

--- src/invoke_training/ui/pages/data_page.py
+++ src/invoke_training/ui/pages/data_page.py
@@ -16,6 +16,10 @@ IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png"]
 
 class DataPage:
     def __init__(self):
+        # The dataset that is currently being edited.
+        self._jsonl_path: str | None = None
+        self._dataset: ImageCaptionJsonlDataset | None = None
+
         logo_path = get_assets_dir_path() / "logo.png"
         with gr.Blocks(title="invoke-training", analytics_enabled=False) as app:
             with gr.Column():
@@ -33,15 +37,15 @@ class DataPage:
                     )
 
             gr.Markdown("# Data Annotation")
-
-            gr.Markdown("To get started, either create a new dataset or load an existing one.")
             gr.Markdown(
                 "Note: This UI creates datasets in `IMAGE_CAPTION_JSONL_DATASET` format. For more information about "
                 "this format see [the docs](https://invoke-ai.github.io/invoke-training/concepts/dataset_formats/)"
             )
 
-            gr.Markdown("## Setup")
-            with gr.Group():
+            # HACK: I use a column as a wrapper to control visbility of this group of UI elements. gr.Group sounds like
+            # a more natural choice for this purpose, but it applies some styling that makes the group look weird.
+            with gr.Column() as select_dataset_group:
+                gr.Markdown("## Setup")
                 # TODO: Expose image_column and caption_column as inputs?
                 self._jsonl_path_textbox = gr.Textbox(
                     label=".jsonl Path",
@@ -57,33 +61,45 @@ class DataPage:
                     )
                 self._load_dataset_button = gr.Button("Load or Create Dataset")
 
-            gr.Markdown("## Add Images")
-            with gr.Group():
-                self._image_source_textbox = gr.Textbox(
-                    label="Image Source",
-                    info="Enter the path to a single image or a directory containing images. If a directory path is "
-                    "passed, it will be searched recursively for image files.",
-                    placeholder="/path/to/image_dir",
-                )
-                self._add_images_button = gr.Button("Add Images")
+            self._select_dataset_group = select_dataset_group
 
-            gr.Markdown("## Edit Captions")
-            with gr.Row():
-                self._cur_example_index = gr.Number(label="Current index", precision=0, interactive=True)
-                self._cur_len_number = gr.Number(label="Dataset length", interactive=False)
-            self._beyond_dataset_limits_warning = gr.Markdown("**Current index is beyond dataset limits.**")
-            self._cur_image = gr.Image(value=None, label="Image", interactive=False, width=500)
-            self._cur_caption = gr.Textbox(label="Caption", interactive=True)
-            with gr.Row():
-                self._save_and_prev_button = gr.Button("Save and Go-To Previous")
-                self._save_and_next_button = gr.Button("Save and Go-To Next")
+            # HACK: I use a column as a wrapper to control visbility of this group of UI elements. gr.Group sounds like
+            # a more natural choice for this purpose, but it applies some styling that makes the group look weird.
+            with gr.Column(visible=False) as edit_dataset_group:
+                with gr.Row():
+                    self._current_jsonl_path = gr.Textbox(label="Currently editing:", interactive=False)
+                    self._change_dataset_button = gr.Button("Change")
+                gr.Markdown("## Add Images")
+                with gr.Group():
+                    self._image_source_textbox = gr.Textbox(
+                        label="Image Source",
+                        info="Enter the path to a single image or a directory containing images. If a directory path "
+                        "is passed, it will be searched recursively for image files.",
+                        placeholder="/path/to/image_dir",
+                    )
+                    self._add_images_button = gr.Button("Add Images")
 
-            gr.Markdown("## Raw JSONL")
-            self._data_jsonl = gr.Code(label="Dataset .jsonl", language="json", interactive=False)
+                gr.Markdown("## Edit Captions")
+                with gr.Row():
+                    self._cur_example_index = gr.Number(label="Current index", precision=0, interactive=True)
+                    self._cur_len_number = gr.Number(label="Dataset length", interactive=False)
+                self._beyond_dataset_limits_warning = gr.Markdown("**Current index is beyond dataset limits.**")
+                self._cur_image = gr.Image(value=None, label="Image", interactive=False, width=500)
+                self._cur_caption = gr.Textbox(label="Caption", interactive=True)
+                with gr.Row():
+                    self._save_and_prev_button = gr.Button("Save and Go-To Previous")
+                    self._save_and_next_button = gr.Button("Save and Go-To Next")
 
+                gr.Markdown("## Raw JSONL")
+                self._data_jsonl = gr.Code(label="Dataset .jsonl", language="json", interactive=False)
+
+            self._edit_dataset_group = edit_dataset_group
             self._app = app
 
             standard_outputs = [
+                self._select_dataset_group,
+                self._edit_dataset_group,
+                self._current_jsonl_path,
                 self._cur_len_number,
                 self._cur_example_index,
                 self._cur_image,
@@ -91,77 +107,70 @@ class DataPage:
                 self._beyond_dataset_limits_warning,
                 self._data_jsonl,
             ]
+
             self._load_dataset_button.click(
                 self._on_load_dataset_button_click,
                 inputs=set([self._jsonl_path_textbox, self._image_column_textbox, self._caption_column_textbox]),
                 outputs=standard_outputs,
             )
+
+            self._change_dataset_button.click(
+                self._on_change_dataset_button_click, inputs=None, outputs=standard_outputs
+            )
             self._save_and_prev_button.click(
                 self._on_save_and_prev_button_click,
-                inputs=set(
-                    [
-                        self._jsonl_path_textbox,
-                        self._image_column_textbox,
-                        self._caption_column_textbox,
-                        self._cur_example_index,
-                        self._cur_caption,
-                    ]
-                ),
-                outputs=standard_outputs,
-            )
-            self._save_and_next_button.click(
-                self._on_save_and_next_button_click,
-                inputs=set(
-                    [
-                        self._jsonl_path_textbox,
-                        self._image_column_textbox,
-                        self._caption_column_textbox,
-                        self._cur_example_index,
-                        self._cur_caption,
-                    ]
-                ),
-                outputs=standard_outputs,
-            )
-            self._add_images_button.click(
-                self._on_add_images_button_click,
-                inputs=set(
-                    [
-                        self._jsonl_path_textbox,
-                        self._image_column_textbox,
-                        self._caption_column_textbox,
-                        self._image_source_textbox,
-                    ]
-                ),
-                outputs=standard_outputs,
-            )
-            self._cur_example_index.input(
-                self._on_cur_example_index_change,
-                inputs=set(
-                    [
-                        self._jsonl_path_textbox,
-                        self._image_column_textbox,
-                        self._caption_column_textbox,
-                        self._cur_example_index,
-                        self._cur_caption,
-                    ]
-                ),
+                inputs=set([self._cur_example_index, self._cur_caption]),
                 outputs=standard_outputs,
             )
 
-    def _update_state(self, dataset: ImageCaptionJsonlDataset, idx: int):
+            self._save_and_next_button.click(
+                self._on_save_and_next_button_click,
+                inputs=set([self._cur_example_index, self._cur_caption]),
+                outputs=standard_outputs,
+            )
+
+            self._add_images_button.click(
+                self._on_add_images_button_click,
+                inputs=set([self._image_source_textbox]),
+                outputs=standard_outputs,
+            )
+
+            self._cur_example_index.input(
+                self._on_cur_example_index_change,
+                inputs=set([self._cur_example_index]),
+                outputs=standard_outputs,
+            )
+
+    def _update_state(self, idx: int):
+        if self._dataset is None or self._jsonl_path is None:
+            return {
+                self._select_dataset_group: gr.Group(visible=True),
+                self._edit_dataset_group: gr.Column(visible=False),
+                self._current_jsonl_path: None,
+                self._cur_len_number: 0,
+                self._cur_example_index: 0,
+                self._cur_image: None,
+                self._cur_caption: None,
+                self._beyond_dataset_limits_warning: gr.Markdown(visible=False),
+                self._data_jsonl: "",
+            }
+
         idx = idx
         image = None
         caption = None
         beyond_limits = True
-        if 0 <= idx and idx < len(dataset):
+        if 0 <= idx and idx < len(self._dataset):
             beyond_limits = False
-            example = dataset[idx]
+            example = self._dataset[idx]
             image = example["image"]
             caption = example["caption"]
 
-        jsonl_str = "\n".join([example.model_dump_json() for example in dataset.examples])
+        jsonl_str = "\n".join([example.model_dump_json() for example in self._dataset.examples])
         return {
-            self._cur_len_number: len(dataset),
+            self._select_dataset_group: gr.Group(visible=self._dataset is None),
+            self._edit_dataset_group: gr.Column(visible=self._dataset is not None),
+            self._current_jsonl_path: str(self._jsonl_path),
+            self._cur_len_number: len(self._dataset),
             self._cur_example_index: idx,
             self._cur_image: image,
             self._cur_caption: caption,
@@ -188,24 +197,24 @@ class DataPage:
             caption_column=data[self._caption_column_textbox] or CAPTION_COLUMN_DEFAULT,
         )
 
-        return self._update_state(dataset, 0)
+        self._jsonl_path = jsonl_path
+        self._dataset = dataset
+        return self._update_state(0)
+
+    def _on_change_dataset_button_click(self):
+        self._jsonl_path = None
+        self._dataset = None
+        return self._update_state(0)
 
     def _on_save_and_go_button_click(self, data: dict, idx_change: int):
-        jsonl_path = Path(data[self._jsonl_path_textbox])
-        dataset = ImageCaptionJsonlDataset(
-            jsonl_path=jsonl_path,
-            image_column=data[self._image_column_textbox] or IMAGE_COLUMN_DEFAULT,
-            caption_column=data[self._caption_column_textbox] or CAPTION_COLUMN_DEFAULT,
-        )
-
         # Update the current caption and re-save the jsonl file.
         idx: int = data[self._cur_example_index]
-        print(f"Updating caption for example {idx} of '{jsonl_path}'.")
+        print(f"Updating caption for example {idx} of '{self._jsonl_path}'.")
         caption = data[self._cur_caption]
-        dataset.examples[idx].caption = caption
-        dataset.save_jsonl()
+        self._dataset.examples[idx].caption = caption
+        self._dataset.save_jsonl()
 
-        return self._update_state(dataset, idx + idx_change)
+        return self._update_state(idx + idx_change)
 
     def _on_save_and_next_button_click(self, data: dict):
         return self._on_save_and_go_button_click(data, 1)
@@ -214,14 +223,7 @@ class DataPage:
         return self._on_save_and_go_button_click(data, -1)
 
     def _on_cur_example_index_change(self, data: dict):
-        jsonl_path = Path(data[self._jsonl_path_textbox])
-        dataset = ImageCaptionJsonlDataset(
-            jsonl_path=jsonl_path,
-            image_column=data[self._image_column_textbox] or IMAGE_COLUMN_DEFAULT,
-            caption_column=data[self._caption_column_textbox] or CAPTION_COLUMN_DEFAULT,
-        )
-
-        return self._update_state(dataset, data[self._cur_example_index])
+        return self._update_state(data[self._cur_example_index])
 
     def _on_add_images_button_click(self, data: dict):
         """Add images to the dataset."""
@@ -229,13 +231,6 @@ class DataPage:
 
         if not image_source_path.exists():
             raise ValueError(f"'{image_source_path}' does not exist.")
-
-        jsonl_path = Path(data[self._jsonl_path_textbox])
-        dataset = ImageCaptionJsonlDataset(
-            jsonl_path=jsonl_path,
-            image_column=data[self._image_column_textbox] or IMAGE_COLUMN_DEFAULT,
-            caption_column=data[self._caption_column_textbox] or CAPTION_COLUMN_DEFAULT,
-        )
 
         # Determine the list of image paths to add to the dataset.
         image_paths = []
@@ -253,21 +248,21 @@ class DataPage:
                     image_paths.append(file_path.resolve())
 
         # Avoid adding duplicate images.
-        cur_image_paths = set([Path(example.image_path) for example in dataset.examples])
+        cur_image_paths = set([Path(example.image_path) for example in self._dataset.examples])
         image_paths = set(image_paths)
         new_image_paths = image_paths - cur_image_paths
         if len(new_image_paths) < len(image_paths):
             print(f"Skipping {len(image_paths) - len(new_image_paths)} images that are already in the dataset.")
 
         # Add the new images to the dataset.
-        print(f"Adding {len(new_image_paths)} images to '{jsonl_path}'.")
+        print(f"Adding {len(new_image_paths)} images to '{self._jsonl_path}'.")
         for image_path in new_image_paths:
-            dataset.examples.append(ImageCaptionExample(image_path=str(image_path), caption=""))
+            self._dataset.examples.append(ImageCaptionExample(image_path=str(image_path), caption=""))
 
         # Save the updated dataset.
-        dataset.save_jsonl()
+        self._dataset.save_jsonl()
 
-        return self._update_state(dataset, 0)
+        return self._update_state(0)
 
     def app(self):
         return self._app
