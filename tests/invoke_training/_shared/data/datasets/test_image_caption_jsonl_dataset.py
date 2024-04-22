1cbbac5164efc57750ee46d8c326323b177776c1
Ryan Dick <ryanjdick3@gmail.com>
2024-04-22 15:48:29 -0400

    Add unit test for ImageCaptionJsonlDataset keep_in_memory=True.

--- tests/invoke_training/_shared/data/datasets/test_image_caption_jsonl_dataset.py
+++ tests/invoke_training/_shared/data/datasets/test_image_caption_jsonl_dataset.py
@@ -23,6 +23,18 @@ def test_image_caption_jsonl_dataset_getitem(image_caption_jsonl):  # noqa: F811
     assert example["caption"] == "caption 0"
 
 
+def test_image_caption_jsonl_dataset_keep_in_memory(image_caption_jsonl):  # noqa: F811
+    dataset = ImageCaptionJsonlDataset(str(image_caption_jsonl), keep_in_memory=True)
+
+    example = dataset[0]
+
+    assert set(example.keys()) == {"image", "id", "caption"}
+    assert isinstance(example["image"], PIL.Image.Image)
+    assert example["image"].mode == "RGB"
+    assert example["id"] == "0"
+    assert example["caption"] == "caption 0"
+
+
 def test_image_caption_jsonl_dataset_get_image_dimensions(image_caption_jsonl):  # noqa: F811
     dataset = ImageCaptionJsonlDataset(str(image_caption_jsonl))
 
