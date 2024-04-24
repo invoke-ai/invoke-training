3760db64d8e182499e139c3132aad17602fcb6cf
Ryan Dick <ryanjdick3@gmail.com>
2024-04-24 16:49:29 -0400

    Add --host and --port args to invoke-train-ui.

--- src/invoke_training/scripts/invoke_train_ui.py
+++ src/invoke_training/scripts/invoke_train_ui.py
@@ -1,11 +1,26 @@
+import argparse
+
 import uvicorn
 
 from invoke_training.ui.app import build_app
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host",
+        default="127.0.0.1",
+        help="The server host. Set `--host 0.0.0.0` to make the app available on your network.",
+    )
+    parser.add_argument("--port", default=8000, type=int, help="The server port.")
+    args = parser.parse_args()
+
     app = build_app()
-    uvicorn.run(app)
+    uvicorn.run(
+        app,
+        host=args.host,
+        port=args.port,
+    )
 
 
 if __name__ == "__main__":
