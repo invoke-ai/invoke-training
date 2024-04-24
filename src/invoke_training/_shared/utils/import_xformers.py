4c672c1d931f5be41b2c7aeaee6f7020b961de74
Ryan Dick <ryanjdick3@gmail.com>
2024-04-24 11:49:07 -0400

    Add xformers as optional dep to pyproject.toml

--- src/invoke_training/_shared/utils/import_xformers.py
+++ src/invoke_training/_shared/utils/import_xformers.py
@@ -4,5 +4,5 @@ def import_xformers():
     except ImportError:
         raise ImportError(
             "xformers is not installed. Either set `xformers = False` in your training config, or install it using "
-            "`pip install xformers>=0.0.23`."
+            '`pip install ".[xformers]"`.'
         )
