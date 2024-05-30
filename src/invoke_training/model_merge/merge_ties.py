bcbeeaff634934893f46387c7ba35d169d36ee31
Ryan Dick <ryanjdick3@gmail.com>
2024-05-30 15:54:52 -0400

    Fix base model vs task model handling in TIES merging implementation.

--- src/invoke_training/model_merge/merge_ties.py
+++ src/invoke_training/model_merge/merge_ties.py
@@ -3,39 +3,44 @@ from peft.utils.merge_utils import ties
 
 
 def merge_ties(
-    state_dicts: list[dict[str, torch.Tensor]],
-    weights: list[float],
+    base_state_dict: dict[str, torch.Tensor],
+    task_state_dicts: list[dict[str, torch.Tensor]],
+    task_weights: list[float],
     density: float = 0.2,
 ) -> torch.Tensor:
     """TIES-Merging algorithm from https://arxiv.org/pdf/2306.01708.
 
     Args:
-        state_dicts (list[dict[str, torch.Tensor]]): A list of state dicts to merge.
-        weights (list[float]): Weights for each state dict. Weights of 1.0 for all state_dicts are recommended as a
-            starting point. The relative weights can be adjusted from there (e.g. [1.0, 1.3, 1.0]). The weights are not
-            normalized like in a basic Lerp merge operation, so deviating too far from a weight of 1.0 may produce
-            unexpected results.
+        base_state_dict (dict[str, torch.Tensor]): The base state dict to merge with.
+        task_state_dicts (list[dict[str, torch.Tensor]]): A list of task-specific state dicts to merge into the base
+            state dict.
+        task_weights (list[float]): Weights for each task state dict. Weights of 1.0 for all task_state_dicts are
+            recommended as a starting point. The weights can be adjusted from there (e.g. [1.0, 1.3, 1.0]). The weights
+            are multipliers applied to the diff between each task_state_dict and the base model.
         density (float, optional): The fraction of values to preserve in the "TRIM" step of TIES. Should be in the
             range [0, 1].
     """
-    if len(state_dicts) < 2:
-        raise ValueError("Must provide >=2 models to merge.")
-
-    if len(state_dicts) != len(weights):
+    if len(task_state_dicts) != len(task_weights):
         raise ValueError("Must provide a weight for each model.")
 
     if density < 0 or density > 1:
         raise ValueError(f"Density must be in the range [0, 1]. Value provided: '{density}'.")
 
+    task_weights = torch.tensor(task_weights)
+
     out_state_dict: dict[str, torch.Tensor] = {}
-    for key in state_dicts[0].keys():
-        # TODO(ryand): The PEFT TIES implementation uses a naive weighting strategy. It could be worth testing a
-        # lerp-like weighting strategy in the "Disjoint Merge" operation.
-        out_state_dict[key] = ties(
-            task_tensors=[state_dict[key] for state_dict in state_dicts],
-            weights=torch.tensor(weights),
+    for key in base_state_dict.keys():
+        base_tensor = base_state_dict[key]
+        # Calculate the diff between each task tensor and the base tensor.
+        task_diff_tensors = [state_dict[key] - base_tensor for state_dict in task_state_dicts]
+
+        merged_diff_tensor = ties(
+            task_tensors=task_diff_tensors,
+            weights=task_weights,
             density=density,
             majority_sign_method="total",
         )
+
+        out_state_dict[key] = base_tensor + merged_diff_tensor
 
     return out_state_dict
--- tests/invoke_training/model_merge/test_merge_ties.py
+++ tests/invoke_training/model_merge/test_merge_ties.py
@@ -24,23 +24,33 @@ def test_merge_ties_raises_on_density_out_of_range():
 
 
 @pytest.mark.parametrize(
-    ["state_dicts", "weights", "expected_state_dict"],
+    ["base_state_dict", "task_state_dicts", "task_weights", "expected_state_dict"],
     [
         # TIES.
         (
+            {"a": torch.tensor([1.0, 2.0]), "b": torch.tensor([3.0, 4.0])},
             [
-                {"a": torch.tensor([1.0, 5.0]), "b": torch.tensor([0.0, 2.0])},
-                {"a": torch.tensor([6.0, 1.0]), "b": torch.tensor([0.0, 3.0])},
+                {"a": torch.tensor([2.0, 7.0]), "b": torch.tensor([3.0, 6.0])},
+                {"a": torch.tensor([7.0, 3.0]), "b": torch.tensor([3.0, 7.0])},
             ],
             [1.0, 1.0],
-            {"a": torch.tensor([6.0, 5.0]), "b": torch.tensor([0.0, 2.5])},
+            # Expected task diff state dict:
+            # {"a": torch.tensor([1.0, 5.0]), "b": torch.tensor([0.0, 2.0])},
+            # {"a": torch.tensor([6.0, 1.0]), "b": torch.tensor([0.0, 3.0])},
+            # Expected merged diff state dict:
+            # {"a": torch.tensor([6.0, 5.0]), "b": torch.tensor([0.0, 2.5])},
+            # Expected final result:
+            {"a": torch.tensor([7.0, 7.0]), "b": torch.tensor([3.0, 6.5])},
         ),
     ],
 )
 def test_merge_ties(
-    state_dicts: list[dict[str, torch.Tensor]],
-    weights: list[float],
+    base_state_dict: dict[str, torch.Tensor],
+    task_state_dicts: list[dict[str, torch.Tensor]],
+    task_weights: list[float],
     expected_state_dict: dict[str, torch.Tensor],
 ):
-    merged_state_dict = merge_ties(state_dicts=state_dicts, weights=weights, density=0.5)
+    merged_state_dict = merge_ties(
+        base_state_dict=base_state_dict, task_state_dicts=task_state_dicts, task_weights=task_weights, density=0.5
+    )
     assert state_dicts_are_close(merged_state_dict, expected_state_dict)
