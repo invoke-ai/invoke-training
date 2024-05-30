547049f8d5d5c4e831e3dfc23d2a32fa2fc43a08
Ryan Dick <ryanjdick3@gmail.com>
2024-05-30 12:16:45 -0400

    Add support for slerp model merging operation.

--- src/invoke_training/model_merge/merger.py
+++ src/invoke_training/model_merge/merger.py
@@ -3,7 +3,7 @@ from typing import Callable
 
 import torch
 
-from invoke_training.model_merge.utils import normalize_weights
+from invoke_training.model_merge.utils.normalize_weights import normalize_weights
 
 # Helper types
 StateDict = dict[str, torch.Tensor]
@@ -11,15 +11,17 @@ WeightedMergeFn = Callable[[torch.Tensor, torch.Tensor, float], torch.Tensor]
 
 
 class WeightedMergeOperation(Enum):
-    WeightedSum = "weighted_sum"
+    # Linear interpolation (aka weighted sum).
+    Lerp = "lerp"
+    # Spherical linear interpolation.
     Slerp = "slerp"
 
 
 def get_merge_fn(operation: WeightedMergeOperation) -> WeightedMergeFn:
-    if operation == WeightedMergeOperation.WeightedSum:
-        return weighted_sum
+    if operation == WeightedMergeOperation.Lerp:
+        return lerp
     elif operation == WeightedMergeOperation.Slerp:
-        raise NotImplementedError("Slerp not implemented.")
+        return slerp
     else:
         raise ValueError(f"Unknown operation: {operation}")
 
@@ -54,5 +56,47 @@ class WeightedModelMerger:
         return out_state_dict
 
 
-def weighted_sum(a: torch.Tensor, b: torch.Tensor, weight_a: float) -> torch.Tensor:
-    return a * weight_a + b * (1 - weight_a)
+def lerp(a: torch.Tensor, b: torch.Tensor, weight_a: float) -> torch.Tensor:
+    """Linear interpolation."""
+    return torch.lerp(a, b, (1.0 - weight_a))
+
+
+def slerp(a: torch.Tensor, b: torch.Tensor, weight_a: float, dot_product_thres=0.9995, epsilon=1e-10):
+    """Spherical linear interpolation."""
+    # TODO(ryand): For multi-dimensional matrices, it might be better to apply slerp on a subset of the dimensions
+    # (e.g. per-row), rather than treating the entire matrix as a single flattened vector.
+
+    # Normalize the vectors.
+    a_norm = torch.linalg.norm(a)
+    b_norm = torch.linalg.norm(b)
+    a_normalized = a / a_norm
+    b_normalized = b / b_norm
+
+    if a_norm < epsilon or b_norm < epsilon:
+        # If either vector is very small, fallback to lerp to avoid weird effects.
+        # TODO(ryand): Is fallback here necessary?
+        return lerp(a, b, weight_a)
+
+    # Dot product of the normalized vectors.
+    # We are effectively treating multi-dimensional tensors as flattened vectors.
+    dot_prod = torch.sum(a_normalized * b_normalized)
+
+    # If the absolute value of the dot product is almost 1, the vectors are ~colinear, so use lerp.
+    if torch.abs(dot_prod) > dot_product_thres:
+        return lerp(a, b, weight_a)
+
+    # Calculate initial angle between v0 and v1.
+    theta_0 = torch.acos(dot_prod)
+
+    # Angle at timestep t.
+    t = 1.0 - weight_a
+    theta_t = theta_0 * t
+
+    sin_theta_0 = torch.sin(theta_0)
+    sin_theta_t = torch.sin(theta_t)
+
+    s0 = torch.sin(theta_0 - theta_t) / sin_theta_0
+    s1 = sin_theta_t / sin_theta_0
+    result = s0 * a + s1 * b
+
+    return result
--- tests/invoke_training/model_merge/test_merger.py
+++ tests/invoke_training/model_merge/test_merger.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 import torch
 
@@ -20,8 +22,9 @@ def test_weighted_sum_merger_raises_on_mismatched_weights():
 
 
 @pytest.mark.parametrize(
-    ["state_dicts", "weights", "expected_state_dict"],
+    ["state_dicts", "weights", "expected_state_dict", "operation"],
     [
+        # Lerp.
         (
             [
                 {"a": torch.tensor(1.0), "b": torch.tensor(2.0)},
@@ -29,7 +32,9 @@ def test_weighted_sum_merger_raises_on_mismatched_weights():
             ],
             [1.0, 1.0],
             {"a": torch.tensor(2.0), "b": torch.tensor(3.0)},
+            WeightedMergeOperation.Lerp,
         ),
+        # Lerp with unbalanced weights.
         (
             [
                 {"a": torch.tensor(1.0), "b": torch.tensor(2.0)},
@@ -37,7 +42,9 @@ def test_weighted_sum_merger_raises_on_mismatched_weights():
             ],
             [1.0, 3.0],
             {"a": torch.tensor(1.0 * 0.25 + 3.0 * 0.75), "b": torch.tensor(2.0 * 0.25 + 4.0 * 0.75)},
+            WeightedMergeOperation.Lerp,
         ),
+        # Lerp with more than 2 state dicts.
         (
             [
                 {"a": torch.tensor(1.0), "b": torch.tensor(2.0)},
@@ -46,11 +53,45 @@ def test_weighted_sum_merger_raises_on_mismatched_weights():
             ],
             [1.0, 1.0, 1.0],
             {"a": torch.tensor(2.0), "b": torch.tensor(3.0)},
+            WeightedMergeOperation.Lerp,
+        ),
+        # Slerp with scalar tensors falls back to lerp.
+        (
+            [
+                {"a": torch.tensor(1.0), "b": torch.tensor(2.0)},
+                {"a": torch.tensor(3.0), "b": torch.tensor(4.0)},
+            ],
+            [1.0, 1.0],
+            {"a": torch.tensor(2.0), "b": torch.tensor(3.0)},
+            WeightedMergeOperation.Slerp,
+        ),
+        # Slerp with colinear vector tensors falls back to lerp.
+        (
+            [
+                {"a": torch.tensor([1.0, 2.0])},
+                {"a": torch.tensor([2.0, 4.0])},
+            ],
+            [1.0, 1.0],
+            {"a": torch.tensor([1.5, 3.0])},
+            WeightedMergeOperation.Slerp,
+        ),
+        # Slerp with orthogonal vector tensors.
+        (
+            [
+                {"a": torch.tensor([1.0, 0.0])},
+                {"a": torch.tensor([0.0, 1.0])},
+            ],
+            [1.0, 1.0],
+            {"a": torch.tensor([math.sin(math.pi / 4), math.sin(math.pi / 4)])},
+            WeightedMergeOperation.Slerp,
         ),
     ],
 )
-def test_weighted_sum_merger(
-    state_dicts: list[dict[str, torch.Tensor]], weights: list[float], expected_state_dict: dict[str, torch.Tensor]
+def test_weighted_merger(
+    state_dicts: list[dict[str, torch.Tensor]],
+    weights: list[float],
+    expected_state_dict: dict[str, torch.Tensor],
+    operation: WeightedMergeOperation,
 ):
-    merged_state_dict = WeightedModelMerger().merge(state_dicts, weights, operation=WeightedMergeOperation.WeightedSum)
+    merged_state_dict = WeightedModelMerger().merge(state_dicts, weights, operation=operation)
     assert state_dicts_are_close(merged_state_dict, expected_state_dict)
