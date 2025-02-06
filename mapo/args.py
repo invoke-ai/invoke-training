c6bc0a0fa5911c54a31e68ff1320abd3a119e650
Ryan Dick <ryanjdick3@gmail.com>
2025-02-06 20:15:51 +0000

    Add timestamp to ourput dir for each run to prevent overwriting logs.

--- mapo/args.py
+++ mapo/args.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 
 
 def parse_args(input_args=None):
@@ -289,5 +290,7 @@ def parse_args(input_args=None):
     env_local_rank = int(os.environ.get("LOCAL_RANK", -1))
     if env_local_rank != -1 and env_local_rank != args.local_rank:
         args.local_rank = env_local_rank
+
+    args.output_dir = os.path.join(args.output_dir, f"{time.time()}")
 
     return args
